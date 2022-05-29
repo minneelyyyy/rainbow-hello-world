ca5a1bf20a7b75de9b2931b59deab328545ab0bf
April Kahny <aprilkahny@gmail.com>
2022-05-29 15:55:28 -0400

    fix integer overflow error

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ fn main() {
 
     let text = if args.is_empty() { "Hello, World!" } else { &args };
     let increment: u16 = 5;
-    let mut hue: u16 = 0;
     let mut index: u16 = 0;
-
+    let mut hue: u16 = 0;
+    
     loop {
         for character in text.chars() {
-            let color = colors::hsv_to_rgb(colors::HSV { h: (hue + increment * index) % 360, s: 60, v: 80 });
+            let color = colors::hsv_to_rgb(
+                colors::HSV {
+                    h: (hue + increment * index) % 360,
+                    s: 60,
+                    v: 80
+                }
+            );
+
             print!("{}", mincolors::rgb!(color.r, color.g, color.b, character));
 
             index += 1;
@@ -21,7 +28,7 @@ fn main() {
         std::io::stdout().flush().expect("failed to flush stdout");
         std::thread::sleep(std::time::Duration::from_millis(100));
 
-        hue = hue + increment;
+        hue = (hue + increment) % 360;
         index = 0;
 
         print!("\r");
